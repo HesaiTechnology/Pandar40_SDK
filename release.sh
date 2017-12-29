a13342a713ede03565997fd4ac4aa3d12ac7a823
yy <bayangyou@hesaihech.com>
2017-12-29 13:26:51 +0800

    update release.sh

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
-cp include/pandoraSDK.h include/point_types.h release/pandoraSDK/
-mkdir -p release/pandoraSDK/lib/`lsb_release -rs`
-cp build/libpandoraSDK.so release/pandoraSDK/lib/`lsb_release -rs`/
+cp include/pandoraSDK.h include/point_types.h sdk/pandoraSDK/
+mkdir -p sdk/pandoraSDK/lib/`lsb_release -rs`
+cp build/libpandoraSDK.so sdk/pandoraSDK/lib/`lsb_release -rs`/
